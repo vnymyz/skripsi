70cbccf914218bf6f7c26a1adeaf1fee71b524e1
The_arms <angga.big@gmail.com>
2023-07-17 17:33:16 +0700

    perbaikan versi rss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,13 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // Contoh penambahan PostCSS dan plugin lainnya
+    postcss: {
+      plugins: [
+        require('postcss-plugin-1'),
+        require('postcss-plugin-2'),
+        // Tambahkan plugin PostCSS lainnya jika diperlukan
+      ],
+    },
   },
-}
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  purge: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
